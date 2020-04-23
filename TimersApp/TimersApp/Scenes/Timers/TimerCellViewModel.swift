1f4a351313398494e00c85b264f5fc89e2e856d7
bandzior157 <bandzior157@gmail.com>
2020-04-23 22:16:53 +0200

    Add TimerCellViewModel with nameText and descriptionText

---
+++ TimersApp/TimersApp/Scenes/Timers/TimerCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TimerCellViewModel.swift
+//  TimersApp
+//
+//  Created by Łukasz Bazior on 23/04/2020.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+struct TimerCellViewModel {
+    var nameText: String
+    var descriptionText: String
+}
