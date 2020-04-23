d9744a0380e5abe9c9ddce78b398848287dc3892
bandzior157 <bandzior157@gmail.com>
2020-04-23 22:41:58 +0200

    Layout TimerTableViewCell

--- TimersApp/TimersApp/Scenes/Timers/TimerTableViewCell.swift
+++ TimersApp/TimersApp/Scenes/Timers/TimerTableViewCell.swift
@@ -12,13 +12,40 @@ class TimerTableViewCell: UITableViewCell {
     
     lazy var nameLabel: UILabel = {
         var label = UILabel()
+        label.textColor = .label
         return label
     }()
     
     lazy var descriptionLabel: UILabel = {
         var label = UILabel()
+        label.textColor = .secondaryLabel
         return label
     }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        let stackView = UIStackView(arrangedSubviews: [nameLabel, descriptionLabel])
+        stackView.alignment = .leading
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        
+        addSubview(stackView)
+        bringSubviewToFront(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        stackView.leftAnchor.constraint(equalTo: safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
+        stackView.rightAnchor.constraint(equalTo: safeAreaLayoutGuide.rightAnchor, constant: -8).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
+    }
     
 }
 
